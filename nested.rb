eb36f9772e8671ed6badc7717a3ded9508f0d28c
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-26 01:51:29 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -100,7 +100,8 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     programmer_hash[:alan_kay][:known_for]
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     binding.pry
      
      
 end
